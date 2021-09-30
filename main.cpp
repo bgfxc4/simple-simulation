5d8a61432ef989ed9ee118cc1786411b6651c174
bgfxc4 <bindermarc@gmx.de>
2021-09-30 22:29:39 +0200

    added bouncing from screen borders

--- main.cpp
+++ main.cpp
@@ -44,19 +44,36 @@ class Bouncer {
 
 class Ball {
 	public:
-	Position vel = Position(0, 0);
+	float radius = 10;
+	Position vel = Position(10, 0);
 	Position pos = Position(100, 0);
 
 	Position gravity = Position(0, 0.2f);
 	
-	void simulate() {
+	void simulate(sf::RenderWindow& window) {
 		vel += gravity;
 		pos += vel;
+		if (pos.x + radius >= window.getSize().x) {
+			pos.x = window.getSize().x - radius;
+			vel.x *= -1;
+		} else if (pos.x - radius < 0) {
+			pos.x = 0 + radius;
+			vel.x *= -1;
+		}
+
+		if (pos.y + radius >= window.getSize().y) {
+			pos.y = window.getSize().y - radius;
+			vel.y *= -1;
+		} else if (pos.y - radius < 0) {
+			pos.y = 0 + radius;
+			vel.y *= -1;
+		}
+
 		std::cout << gravity.y << std::endl;
 	}
 
 	void draw(sf::RenderWindow& window) {
-		sf::CircleShape circle(10);
+		sf::CircleShape circle(radius);
 		circle.setFillColor(sf::Color(150, 50, 250));
 		circle.setPosition(pos.x, pos.y);
 		window.draw(circle);
@@ -67,7 +84,7 @@ std::vector<Ball> balls;
 
 void draw(sf::RenderWindow& window) {
 	for (Ball& b : balls) {
-		b.simulate();
+		b.simulate(window);
 		b.draw(window);
 	}
 }
